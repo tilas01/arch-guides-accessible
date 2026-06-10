53cbc7a43c4423bc7258b468c436b8a4ec71fb67
tilas01 <95286414+tilas01@users.noreply.github.com>
2026-06-11 02:50:38 +1000

    feat: add main.rs wrappers for scarecrow and kernel-watcher binaries

---
+++ security-tools/kernel-watcher/src/main.rs
@@ -0,0 +1,6 @@
+use kernel_watcher::start_watcher;
+
+fn main() {
+    println!("Starting standalone Kernel Watcher...");
+    start_watcher();
+}
---
+++ security-tools/scarecrow/src/main.rs
@@ -0,0 +1,6 @@
+use scarecrow::start_scarecrow;
+
+fn main() {
+    println!("Starting standalone Libre-Cyber-ScareCrow...");
+    start_scarecrow();
+}
